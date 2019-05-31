d4f771e681430a1d0d0b426dce61bc85a3363bc4
Devin Sipal <dsipal@unca.edu>
2019-05-31 07:38:40 -0400

    testing mongo search

--- app/server/modules/account-manager.js
+++ app/server/modules/account-manager.js
@@ -95,7 +95,12 @@ exports.validatePasswordKey = function(passKey, ipAddress, callback)
 exports.addPoints = function(subid, amount){
 	console.log(subid,amount);
 
-	console.log(accounts.findOne({_id:subid}));
+
+	accounts.findOne({_id:subid}).then(function(o){
+			console.log(o);
+	})
+
+	console.log();
 
 	accounts.findOneAndUpdate({_id:subid}, {$inc:{points:10*amount}});
 };
