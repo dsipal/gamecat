4b1c5b7844e33639b33f2ee3a26396b377925eac
Devin Sipal <dsipal@unca.edu>
2019-06-14 02:15:47 -0400

    removed lingering references to account-manager.js

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,6 +1,5 @@
 //TODO clean up code in passport.js, capitalization and wording of text shown to user.
 var LocalStrategy = require('passport-local').Strategy;
-var AM = require('../server/modules/account-manager');
 var mongoose = require('mongoose');
 var User = require('../server/models/User');
 
