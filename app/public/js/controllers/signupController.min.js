039e4f329221de5e3945eef69512dbeb1d3f09c8
Devin Sipal <dsipal@unca.edu>
2019-07-06 19:39:27 -0400

    add assets

---
+++ app/public/js/controllers/signupController.min.js
@@ -0,0 +1,1 @@
+function SignupController(){$("#account-form-btn1").click(function(){window.location.href="/"}),$(".modal-alert #ok").click(function(){setTimeout(function(){window.location.href="/"},300)})}
