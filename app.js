132b8627693d98c155a1cf309cfcc8ccf3c5360d
Devin Sipal <dsipal@unca.edu>
2019-07-26 02:40:46 -0400

    removed landing

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const dotenv = require('dotenv');
 const compression = require('compression');
 dotenv.config();
 
-
-
 // create instance of express server //
 var app = express();
 app.use(helmet());
@@ -24,11 +22,8 @@ app.use(compression());
 app.set('port', process.env.PORT || 8080);
 app.use(secure);
 
-
-
 // route files //
 const index = require('./app/server/routes/index.js');
-const landing = require('./app/server/routes/landing.js');
 const account = require('./app/server/routes/account');
 const shop = require('./app/server/routes/shop');
 const offers = require('./app/server/routes/offers');
@@ -52,57 +47,57 @@ app.set('view engine', 'hbs');
 
 
 // // TODO use below expression to limit ips that access /postback and /admin
-// app.use('/offers/postback', function(req, res, next) {
-//     // filtering here, calls `res` method to stop progress or calls `next` to proceed
-//     let ip = req.ip ||
-//         req.headers['x-forwarded-for'] ||
-//         req.connection.remoteAddress ||
-//         req.socket.remoteAddress ||
-//         req.connection.socket.remoteAddress;
-//     if(ip.substr(0,7) === "::ffff:"){
-//         ip = ip.substr(7);
-//     }
-//     // The IP from the CPA site
-//     if (ip === '54.204.57.82') {
-//         next();
-//     } else {
-//         res.end();
-//     }
-// });
-//
-// app.use('/offers/pwnpostback', function(req, res, next) {
-//     // filtering here, calls `res` method to stop progress or calls `next` to proceed
-//     let ip = req.ip ||
-//         req.headers['x-forwarded-for'] ||
-//         req.connection.remoteAddress ||
-//         req.socket.remoteAddress ||
-//         req.connection.socket.remoteAddress;
-//     if(ip.substr(0,7) === "::ffff:"){
-//         ip = ip.substr(7);
-//     }
-//     // The IP from the CPA site
-//     if (ip === '35.196.95.104' || ip === '35.196.169.46') {
-//         next();
-//     } else {
-//         res.end();
-//     }
-// });
-//
-// app.use('/admin/*', function(req, res, next) {
-//     // filtering here, calls `res` method to stop progress or calls `next` to proceed
-//     let ip = req.ip ||
-//         req.headers['x-forwarded-for'] ||
-//         req.connection.remoteAddress ||
-//         req.socket.remoteAddress ||
-//         req.connection.socket.remoteAddress;
-//
-//     // Our IPs
-//     if (ip === '0.0.0.0') {
-//         next();
-//     } else {
-//         res.end();
-//     }
-// });
+app.use('/offers/postback', function(req, res, next) {
+    // filtering here, calls `res` method to stop progress or calls `next` to proceed
+    let ip = req.ip ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    if(ip.substr(0,7) === "::ffff:"){
+        ip = ip.substr(7);
+    }
+    // The IP from the CPA site
+    if (ip === '54.204.57.82') {
+        next();
+    } else {
+        res.end();
+    }
+});
+
+app.use('/offers/pwnpostback', function(req, res, next) {
+    // filtering here, calls `res` method to stop progress or calls `next` to proceed
+    let ip = req.ip ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    if(ip.substr(0,7) === "::ffff:"){
+        ip = ip.substr(7);
+    }
+    // The IP from the CPA site
+    if (ip === '35.196.95.104' || ip === '35.196.169.46') {
+        next();
+    } else {
+        res.end();
+    }
+});
+
+app.use('/admin/*', function(req, res, next) {
+    // filtering here, calls `res` method to stop progress or calls `next` to proceed
+    let ip = req.ip ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    // Our IPs
+    if (ip === '0.0.0.0') {
+        next();
+    } else {
+        res.end();
+    }
+});
 
 
 // set up view handling //
@@ -117,6 +112,7 @@ app.use(cookieParser(process.env.COOKIEPARSER_SECRET));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//connect to mongodb instance
 mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser: true,
     useFindAndModify: false
@@ -142,24 +138,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // set up the router //
-//require('./app/server/routes')(app, passport);
+app.use('/', index);
+app.use('/account', account);
+app.use('/shop', shop);
+app.use('/offers', offers);
+app.use('/admin', admin);
 
-
-if(process.env.NODE_ENV === 'live'){
-    app.use('/', landing);
-} else {
-    app.use('/', index);
-    app.use('/account', account);
-    app.use('/shop', shop);
-    app.use('/offers', offers);
-    app.use('/admin', admin);
-}
-
+//internal server errors
 app.use(function(err, req, res, next) {
     console.log(err);
     return res.status(500).send({ error: err });
 });
 
+//404 errors
 app.use(function(req, res, next) {
     return res.status(404).render('404');
 });
