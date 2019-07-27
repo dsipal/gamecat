4d9d4408d24f47705f417410cbbe17c572124d2b
Devin Sipal <dsipal@unca.edu>
2019-07-27 02:48:47 -0400

    update order schema

--- app/server/models/Order.js
+++ app/server/models/Order.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const order = new mongoose.Schema({
         prize: {type: mongoose.Schema.ObjectId, ref: 'Prize'},
-        option: {
-            point_cost: Number,
-            dollar_value: String
-        },
+        option: {type: Number},
         user: {type: mongoose.Schema.ObjectId, ref: 'User'},
         status: {type: String},
         order_date: {type: Date},
