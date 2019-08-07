9fe67e833997d51a55b1cd7028aee0d807f4f046
Devin Sipal <dsipal@unca.edu>
2019-08-07 15:42:11 -0400

    remove req.user.username from console.logs

--- app/server/modules/authLimiter.js
+++ app/server/modules/authLimiter.js
@@ -2,15 +2,15 @@ module.exports.ensureAuthenticated = function(){
     //TODO add check to see if user has verified email && add correct error response
     return function(req, res, next){
         if(!req.isAuthenticated || !req.isAuthenticated()){
-            console.log('Failed login attempt from ' + req.user.username);
+            console.log('Failed login attempt');
             //res.status(401).send('not-authenticated');
             res.redirect('/login');
         } else if(req.user.rank === 'new'){
-            console.log('Non-verified email login attempt from ' + req.user.username);
+            console.log('Non-verified email login attempt');
             //res.status(401).send('not-verified');
             res.redirect('/login/unverified');
         } else if(req.user.rank === 'social-new') {
-            console.log('Unfinished social registration login attempt from ' + req.user.username);
+            console.log('Unfinished social registration login attempt');
             res.redirect('/login/finalize');
         }
         else {
