3e15733542fe547598cdcf83a39732ba8a603240
Devin Sipal <dsipal@unca.edu>
2019-08-08 16:28:50 -0400

    fixing ipformatter

--- app/server/modules/ipFormatter.js
+++ app/server/modules/ipFormatter.js
@@ -1,20 +1,6 @@
 function getIP(req){
-    let ip = req.headers['x-forwarded-for']
-        || req.connection.remoteAddress
-        || req.socket.remoteAddress
-        || req.connection.socket.remoteAddress;
-
-    if(ip.substr(0,7) === "::ffff:"){
-        ip = ip.substr(7);
-    }
-    if(ip.includes(',')){
-        let ipArr = ip.split(', ');
-        ip = ipArr[0];
-    }
-    console.log(req.header['CF-Connecting-IP']);
-    console.log(JSON.stringify(req.headers));
-    let country_code = req.header['CF-IPCountry'];
-    console.log(country_code);
+    let ip = req.header['cf-connecting-ip']
+    let country_code = req.header['cf-ipcountry'];
     return [ip, country_code];
 }
 module.exports = getIP;
