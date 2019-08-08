622c9527a543fefde008ce672025ab4e826a8eaf
Devin Sipal <dsipal@unca.edu>
2019-08-08 16:31:13 -0400

    back to testing

--- app/server/modules/ipFormatter.js
+++ app/server/modules/ipFormatter.js
@@ -1,6 +1,9 @@
 function getIP(req){
+    console.log(JSON.stringify(req.headers));
     let ip = req.header['cf-connecting-ip'];
     let country_code = req.header['cf-ipcountry'];
+    console.log(ip);
+    console.log(country_code);
     return [ip, country_code];
 }
 module.exports = getIP;
