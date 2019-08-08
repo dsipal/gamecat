0697bcf04ef41e62692412e63f3ba3648ea32f96
Devin Sipal <dsipal@unca.edu>
2019-08-08 16:29:00 -0400

    add semicolon

--- app/server/modules/ipFormatter.js
+++ app/server/modules/ipFormatter.js
@@ -1,5 +1,5 @@
 function getIP(req){
-    let ip = req.header['cf-connecting-ip']
+    let ip = req.header['cf-connecting-ip'];
     let country_code = req.header['cf-ipcountry'];
     return [ip, country_code];
 }
