94ab12d23fb7e73cfb02be137098a636ccb53f3c
Devin Sipal <dsipal@unca.edu>
2019-08-08 16:41:10 -0400

    testing testing

--- app/server/routes/offers.js
+++ app/server/routes/offers.js
@@ -7,9 +7,9 @@ const Game = require('../models/Game');
 const ipFormatter = require('../modules/ipFormatter');
 
 router.get('/', authLimiter.ensureAuthenticated(), async function(req, res){
-    let country_code = req.header['CF-IPCountry'];
+    let country_code = req.header['cf-ipcountry'];
     console.log(req.headers);
-    console.log(req.header['CF-IPCountry']);
+    console.log(req.header['cf-ipcountry']);
     const offers = await getOffers(country_code, req.user._id);
 
     return res.render('offers/quests', {
