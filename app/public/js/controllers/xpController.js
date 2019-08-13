264b5e01d0d33fa81e933487af2a9603cc737059
Devin Sipal <dsipal@unca.edu>
2019-08-12 20:01:53 -0400

    changed xp bar display, styled more.

--- app/public/js/controllers/xpController.js
+++ app/public/js/controllers/xpController.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
     let xpBar = $('.xp-bar');
     let bonusElem = $('.bonus-amount');
     let levelElem = $('#level');
-    let userLevel = levelElem.data('level');
-    let userXP = levelElem.data('cur-exp');
+    let userLevel = parseInt(levelElem.data('level'));
+    let userXP = parseInt(levelElem.data('cur-exp'));
 
-    console.log(bonusElem);
-    bonusElem.text('+' + ((userLevel - 1) * 0.025) * 100 + '%');
-    let requiredXP = 600 + ((userLevel-1) * 400);
+    const bonusAmount = (((parseInt(userLevel) - 1) * 0.025) * 100).toFixed(2);
+    console.log('bonus: ' + bonusAmount);
+    let bonusString = String('+' + bonusAmount  + '%');
+    bonusElem.text(bonusString);
+    let requiredXP = Math.floor(600 + (userLevel * 400));
     let barPercentage = Math.floor((userXP/requiredXP)*100)+'%';
-    xpBar.width(barPercentage);
+    xpBar.css('width', barPercentage);
     xpBar.text(userXP + '/' + requiredXP + ' (' + barPercentage + ')');
 });
 
