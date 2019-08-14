778338aa2066eb1f16eeb65614b8f3e332077b88
Devin Sipal <dsipal@unca.edu>
2019-08-14 18:35:02 -0400

    add page titles

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -5,6 +5,7 @@ let router = express.Router();
 const authLimiter = require('../modules/authLimiter');
 const Event =  require('../models/Event');
 let Prize = require('../models/Prize');
+
 router.get('/', async function(req, res){
     let modifierText=1;
     let prizes = await Prize.find({}).limit(8);
@@ -14,9 +15,9 @@ router.get('/', async function(req, res){
                 modifierText = (event.modifier+1);
             }
             if(req.user){
-                return res.render('index/home', {udata: req.user, event: event, modifierText: modifierText});
+                return res.render('index/home', {udata: req.user, event: event, modifierText: modifierText, pageTitle: '- Home Page'});
             }else{
-                return res.render('index/index', {udata: req.user, event: event, prizes: prizes, modifierText: modifierText});
+                return res.render('index/index', {udata: req.user, event: event, prizes: prizes, modifierText: modifierText, pageTitle: '- Rewards for Gamers'});
             }
         })
         .catch(function(err){
@@ -24,10 +25,6 @@ router.get('/', async function(req, res){
         console.log(err);
         return res.status(500).send(err);
     });
-
-
-
-
 
 });
 
